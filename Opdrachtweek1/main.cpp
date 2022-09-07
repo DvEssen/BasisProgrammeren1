a74cc39025bf2082eb1a8e5be107bb680aef2bfd
Daniël Essen, van <s1185953@student.windesheim.nl>
2022-09-07 13:15:35 +0200

    first code lesson

---
+++ Opdrachtweek1/main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+	int b;
+
+	a = 42;
+	scanf_s("%d", &b);
+	printf("getal %d \n", a+b);
+	
+	if (b = 10) {
+		printf("mooi getal");
+	}
+	else {
+		printf("geen 10");
+	}
+	return 0;
+}
